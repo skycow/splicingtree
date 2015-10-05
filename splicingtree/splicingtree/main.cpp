d1a50f954a07ccbbe61a308241eaf093dbce5ba0
skycow <skyler.cowley@gmail.com>
2015-10-05 10:30:17 -0600

    201510051029

--- splicingtree/splicingtree/main.cpp
+++ splicingtree/splicingtree/main.cpp
@@ -1,25 +1,45 @@
 #include<iostream>
 #include<fstream>
+#include<sstream>
+#include<string>
 
 using namespace std;
 
+int cost(string*, string[2], string);
+
 int main() {
 
-	double input[3][25];
+	//enum indexNames {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, a, b, c, d, e, f, g, i, j, k, l };
 
+	string name[30];
+	double input[2][30];
+	string line;
 	ifstream fin("input.txt");
 
-	if (fin.good()) {
-		cout << "We're good" << endl;
-		int test;
-		fin >> test;
-		cout << test << endl;
+	if (fin.is_open()) {
+		int count = 0;
+		while (getline(fin, line)) {
+			istringstream individual(line);
+			individual >> name[count];
+			individual >> input[0][count];
+			individual >> input[1][count];
+			count = (count + 1);
+		}
 	}
 	else {
-		cout << "We're not good" << endl;
+		cout << "Unable to open file." << endl;
 	}
 
+	string npe1 = "12v3v4v5v6v7v8v9vavbvcvdvevfvgvivjvkvlv";
+	int area;
+	area = cost(name, input, npe1);
+
 	
+
+	return 0;
+}
+
+int cost(string* name, string** input, string npe) {
 
 	return 0;
 }
